778658f0ad19f4f43d49f5edb02b2ffdc52a4c1c
Ryca77 <ryan.carter77@gmail.com>
2016-06-28 20:09:51 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -4,18 +4,42 @@ $('.programme-list').on('click', function() {
 	$('.programmes').show()
 });
 
-$('.prog1, .prog2, prog3').on('click', function() {
-	$('.programme-list').text("prog")
+$('.programme-list li').on('click', function() {
+	var progSelect = $(this).text()
+	console.log(progSelect)
+	$('.programme-list').text(progSelect)
 	$('.prog1, .prog2, prog3').hide()
+
+	if ($('.programme-list').text() === 'Scholarship') {
+		console.log('highlight logo1')
+	}
+	else if ($('.programme-list').text() === 'Loan') {
+		console.log('highlight logo2')
+	}
+	else if ($('.programme-list').text() === 'Grant') {
+		console.log('highlight logo3')
+	}
 });
 
 $('.education-list').on('click', function() {
 	$('.educations').show()
 });
 
-$('.edu1, .edu2, edu3').on('click', function() {
-	$('.education-list').text("edu")
+$('.education-list li').on('click', function() {
+	var eduSelect = $(this).text()
+	console.log(eduSelect)
+	$('.education-list').text(eduSelect)
 	$('.edu1, .edu2, edu3').hide()
+
+	if ($('.education-list').text() === 'Further') {
+		console.log('highlight logo4')
+	}
+	else if ($('.education-list').text() === 'Secondary') {
+		console.log('highlight logo5')
+	}
+	else if ($('.education-list').text() === 'Primary') {
+		console.log('highlight logo6')
+	}
 });
 
 });
