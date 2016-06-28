924befe1131eee975cab3212f48d8a54c8136c64
Ryca77 <ryan.carter77@gmail.com>
2016-06-28 14:22:56 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -4,16 +4,18 @@ $('.programme-list').on('click', function() {
 	$('.programmes').show()
 });
 
-$('.programme-list').mouseout(function() {
-	$('.programmes').hide()
+$('.prog1, .prog2, prog3').on('click', function() {
+	$('.programme-list').text("prog")
+	$('.prog1, .prog2, prog3').hide()
 });
 
 $('.education-list').on('click', function() {
 	$('.educations').show()
 });
 
-$('.education-list').mouseout(function() {
-	$('.educations').hide()
+$('.edu1, .edu2, edu3').on('click', function() {
+	$('.education-list').text("edu")
+	$('.edu1, .edu2, edu3').hide()
 });
 
 });
