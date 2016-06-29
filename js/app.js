f7dfc0a609e52d6031a44921a698a163613f3210
Ryca77 <ryan.carter77@gmail.com>
2016-06-29 11:54:57 +0100

    js/app.js

--- js/app.js
+++ js/app.js
@@ -4,20 +4,31 @@ $('.programme-list').on('click', function() {
 	$('.programmes').show()
 });
 
-$('.programme-list li').on('click', function() {
+$('.programmes li').on('click', function() {
 	var progSelect = $(this).text()
 	console.log(progSelect)
 	$('.programme-list').text(progSelect)
-	$('.prog1, .prog2, prog3').hide()
+	$('.programmes').hide()
+});
 
+$('.search').on('click', function() {
 	if ($('.programme-list').text() === 'Scholarship') {
 		console.log('highlight logo1')
+		$('.logo1, .logo2, .logo7, .logo8').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
+		$('.logo3, .logo4, .logo9, .logo10').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 	else if ($('.programme-list').text() === 'Loan') {
 		console.log('highlight logo2')
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo1, .logo2, .logo7, .logo8').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
+		$('.logo3, .logo4, .logo9, .logo10').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 	else if ($('.programme-list').text() === 'Grant') {
 		console.log('highlight logo3')
+		$('.logo3, .logo4, .logo9, .logo10').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo1, .logo2, .logo7, .logo8').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 });
 
@@ -25,21 +36,36 @@ $('.education-list').on('click', function() {
 	$('.educations').show()
 });
 
-$('.education-list li').on('click', function() {
+$('.educations li').on('click', function() {
 	var eduSelect = $(this).text()
 	console.log(eduSelect)
 	$('.education-list').text(eduSelect)
-	$('.edu1, .edu2, edu3').hide()
+	$('.educations').hide()
+});
 
+$('.search').on('click', function() {
 	if ($('.education-list').text() === 'Further') {
 		console.log('highlight logo4')
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo3, .logo4, .logo9, .logo10').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 	else if ($('.education-list').text() === 'Secondary') {
 		console.log('highlight logo5')
+		$('.logo3, .logo4').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
+		$('.logo9, .logo10').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 	else if ($('.education-list').text() === 'Primary') {
 		console.log('highlight logo6')
+		$('.logo9, .logo10').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+		$('.logo5, .logo6, .logo11, .logo12').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
+		$('.logo3, .logo4').css({"webkit-filter" : "grayscale(100%) opacity(0.3)"})
 	}
 });
+
+/*
+$('.search').on('click', function() {
+	$('.logos li').css({"webkit-filter" : "grayscale(0%) opacity(1)"})
+});*/
 
 });
